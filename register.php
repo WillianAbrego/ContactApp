ca975f5fb47fc1b08e803f7f59fe55fcfd5c5e12
WillianAbrego <willianantonioabrego@gmail.com>
2022-12-11 20:02:36 -0600

    Create user

--- register.php
+++ register.php
@@ -5,21 +5,28 @@ require "database.php";
 $error = null;
 
 if ($_SERVER["REQUEST_METHOD"]=="POST"){
-  if(empty($_POST["name"]) || empty($_POST["phone_number"])){
-    $error = "Please fill all the fields.";
-  }else if(strlen($_POST["phone_number"])<9){
-    $error = "Phone number must be at least 9 characters."; 
-  } else{
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
-  
-    $statement = $conn->prepare("INSERT INTO contacts (name,phone_number) VALUES (:name, :phone_number)");
-    $statement->bindParam(":name",$_POST["name"]);
-    $statement->bindParam(":phone_number",$_POST["phone_number"]);
+  if ((empty($_POST["name"])) || (empty($_POST["email"])) || (empty($_POST["password"]))) {
+    $error= "Please fill all the fields.";
+  } else if (!str_contains($_POST["email"],"@")) {
+    $error = "Email format is incorrect.";
+  }else{
+    $statement = $conn ->prepare("SELECT * FROM users WHERE email = :email");
+    $statement->bindParam(":email", $_POST["email"]);
     $statement->execute();
-    header("Location: home.php");
-  }
 
+    if ($statement->rowCount()>0) {
+      $error="This email is taken.";
+    }else{
+      $conn
+      ->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)")
+      ->execute([
+        ":name"=>$_POST["name"],
+        ":email"=>$_POST["email"],
+        ":password"=>password_hash($_POST["password"],PASSWORD_BCRYPT),
+      ]);
+      header("Location: home.php");
+    }
+  }
 }
 
 ?>
@@ -30,14 +37,14 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">Add New Contact</div>
+        <div class="card-header">Register</div>
         <div class="card-body">
           <?php if ($error): ?>
             <p class="text-danger">
               <?= $error ?>
             </p>
           <?php endif ?>
-          <form method="POST" action="add.php">
+          <form method="POST" action="register.php">
             <div class="mb-3 row">
               <label
                 for="name"
@@ -60,19 +67,39 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
 
             <div class="mb-3 row">
               <label
-                for="phone_number"
+                for="email"
                 class="col-md-4 col-form-label text-md-end"
-                >Phone Number</label
+                >Email</label
               >
 
               <div class="col-md-6">
                 <input
-                  id="phone_number"
-                  type="tel"
+                  id="email"
+                  type="email"
                   class="form-control"
-                  name="phone_number"
+                  name="email"
                   required
-                  autocomplete="phone_number"
+                  autocomplete="email"
+                  autofocus
+                />
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <label
+                for="email"
+                class="col-md-4 col-form-label text-md-end"
+                >Password</label
+              >
+
+              <div class="col-md-6">
+                <input
+                  id="password"
+                  type="password"
+                  class="form-control"
+                  name="password"
+                  required
+                  autocomplete="password"
                   autofocus
                 />
               </div>
--- sql/setup.sql
+++ sql/setup.sql
@@ -4,10 +4,27 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
-CREATE TABLE contacts(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    phone_number VARCHAR(255)
-);
+CREATE TABLE
+    users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        password VARCHAR(255)
+    );
+
+CREATE TABLE
+    contacts(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        phone_number VARCHAR(255)
+    );
+
+INSERT INTO
+    users (name, email, password)
+VALUES (
+        "test",
+        "test@test.com",
+        "1235"
+    );
 
 INSERT INTO contacts (name,phone_number) VALUES("pepe","3456721");
